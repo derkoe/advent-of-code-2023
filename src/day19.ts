d84ff9b3601cbf6cc30f17acd54298d7ce683405
Christian Köberl <christian.koeberl@porscheinformatik.com>
2023-12-19 23:00:16 +0100

    Day 19 - Part 2

--- src/day19.ts
+++ src/day19.ts
@@ -117,5 +117,104 @@ let part1 = system.parts
   .filter((part) => runWorkflow(system.workflows, part))
   .map((part) => part.x + part.m + part.a + part.s)
   .reduce((a, b) => a + b, 0);
-
 console.log("Part 1:", part1);
+
+type Range = {
+  from: Part;
+  to: Part;
+};
+
+function findAcceptedRanges(
+  workflows: {
+    [key: string]: Workflow;
+  },
+  currentWorkflow: string,
+  ranges: Range[]
+): Range[] {
+  if (currentWorkflow === "A") {
+    return ranges;
+  }
+  if (currentWorkflow === "R") {
+    return [];
+  }
+
+  let workflow = workflows[currentWorkflow];
+  let acceptedRanges: Range[] = [];
+  while (ranges.length > 0) {
+    for (let instruction of workflow.instructions) {
+      if (ranges.length === 0) {
+        break;
+      }
+      const range = ranges.pop()!;
+      const subRanges: Range[] = [];
+      if (instruction.operation === "<") {
+        if (range.to[instruction.field] < instruction.value) {
+          subRanges.push(range);
+        } else if (range.from[instruction.field] < instruction.value) {
+          subRanges.push({
+            from: range.from,
+            to: {
+              ...range.to,
+              [instruction.field]: instruction.value - 1,
+            },
+          });
+          ranges.push({
+            from: {
+              ...range.from,
+              [instruction.field]: instruction.value,
+            },
+            to: range.to,
+          });
+        }
+      } else if (instruction.operation === ">") {
+        if (range.from[instruction.field] > instruction.value) {
+          subRanges.push(range);
+        } else if (range.to[instruction.field] > instruction.value) {
+          subRanges.push({
+            from: {
+              ...range.from,
+              [instruction.field]: instruction.value + 1,
+            },
+            to: range.to,
+          });
+          ranges.push({
+            from: range.from,
+            to: {
+              ...range.to,
+              [instruction.field]: instruction.value,
+            },
+          });
+        }
+      }
+      if (subRanges.length > 0) {
+        acceptedRanges = acceptedRanges.concat(
+          findAcceptedRanges(workflows, instruction.target, subRanges)
+        );
+      }
+    }
+    acceptedRanges = acceptedRanges.concat(
+      findAcceptedRanges(workflows, workflow.fallback, ranges)
+    );
+  }
+  return acceptedRanges;
+}
+
+let ranges = findAcceptedRanges(system.workflows, "in", [
+  {
+    from: { x: 1, m: 1, a: 1, s: 1 },
+    to: { x: 4000, m: 4000, a: 4000, s: 4000 },
+  },
+]);
+
+console.log(
+  "Part 2:",
+  ranges.reduce(
+    (sum, range) =>
+      sum +
+      (range.to.a - range.from.a + 1) *
+        (range.to.m - range.from.m + 1) *
+        (range.to.s - range.from.s + 1) *
+        (range.to.x - range.from.x + 1),
+    0
+  )
+);
